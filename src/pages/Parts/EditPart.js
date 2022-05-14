cdd49f1f798e939de1dfb398f9759bd78851696a
Nuno9180580 <9180580@esmad.ipp.pt>
2022-05-14 15:25:03 +0100

    now done

--- src/pages/Parts/EditPart.js
+++ src/pages/Parts/EditPart.js
@@ -1,13 +1,23 @@
-import { Box, Heading, Text, VStack } from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
+import { Box, Button, Heading, Text, VStack } from "@chakra-ui/react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const EditPart = () => {
   let { name, type, price } = useParams();
+  const navigate = useNavigate();
   return (
     <VStack w="full" h="full" justify={"center"} align="center" spacing={8}>
-      <Heading>{name}</Heading>
-      <Text>{type}</Text>
-      <Text>{price}</Text>
+      <VStack justify={"center"} align="center" spacing={8}>
+        <Heading>{name}</Heading>
+        <Text>{type}</Text>
+        <Text>{price}</Text>
+      </VStack>
+      <Button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Go Back
+      </Button>
     </VStack>
   );
 };
